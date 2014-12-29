c178b32104c7116c6416276575785892a717dd04
jia-long <995690029@qq.com>
2014-12-29 22:29:09 +0800

    modify Page.java

--- javaEE/Page.java
+++ javaEE/Page.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Page<T extends Serializable> {
-	private int pageIndex; // 页面索引号
+	private int pageIndex; // 页面索引号 
 
 	private int pageCount; // 页面总数
 
